c0d1829de801a14514eaefa2cdc4705eaa4272f9
Olivier DAZEL <olivier.dazel@univ-lemans.fr>
2016-06-23 13:43:30 +0200

    Add mat porous 204 & 203

---
+++ Mat_porous_203.m
@@ -0,0 +1,22 @@
+%
+%	Type: Screen (2)
+%	Reference : Allard & Atalla 2009, Table 11.4
+
+porous_model.eqf='JCA';
+porous_model.frame='structural';
+porous_model.aniso='no';
+porous_model.typical_thickness=.8;
+
+phi=0.8;
+sig=3.2e6;
+alpha=2.56;
+LCV=6e-6;
+LCT=24e-6;
+rho_1=125;
+nu=0.3;
+young=2.6e6;
+cisaill=.5*young/(1+nu);
+eta=0.1;
+
+
+
---
+++ Mat_porous_204.m
@@ -0,0 +1,21 @@
+%
+%	Type: Blanket (1)
+%	Reference : Allard & Atalla 2009, Table 11.4
+
+porous_model.eqf='JCA';
+porous_model.frame='structural';
+porous_model.aniso='no';
+
+phi=0.98;
+sig=34e3;
+alpha=1.18;
+LCV=60e-6;
+LCT=86e-6;
+rho_1=41;
+nu=0.3;
+young=286e3;
+cisaill=.5*young/(1+nu)
+eta=0.015;
+
+gfdgfd
+
